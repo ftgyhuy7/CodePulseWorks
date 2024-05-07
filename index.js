554baf8f8b37013c784db0a2e2843bb61b60d0e0
ftgyhuy7 <gs1josqueza@hotmail.com>
2024-05-08 05:50:45 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function minPathSum(grid) {
-  const m = grid.length;
-  const n = grid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  dp[0][0] = grid[0][0];
-  for (let i = 1; i < m; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0];
-  }
-  for (let j = 1; j < n; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j];
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+function maximalSquare(matrix) {
+  if (matrix.length === 0 || matrix[0].length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from({ length: rows + 1 }, () =>
+    Array.from({ length: cols + 1 }, () => 0),
+  );
+  let maxSquare = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+        maxSquare = Math.max(maxSquare, dp[i][j]);
+      }
     }
   }
-  return dp[m - 1][n - 1];
+  return maxSquare * maxSquare;
 }
